1bc503d8a87bb6f8097f7f9a4f80552db8a69ddc
sudhanshu <sp7017184@gmail.com>
2026-08-16 17:32:34 +0530

    fixed the origin and added the frontend url

--- numlookup-backend/src/server.ts
+++ numlookup-backend/src/server.ts
@@ -2,11 +2,12 @@ import app from "./app";
 import "./config/env";
 import prisma from "./prisma/client";
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
 process.on("SIGINT", async () => {
   console.log("Shutting down...");
 
